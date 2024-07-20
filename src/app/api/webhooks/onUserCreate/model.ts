0baacb243da53469a4861e94f80262f86332a4e8
do0dleman <nikita.obrazcovs@gmail.com>
2024-07-20 09:09:02 +0300

    fixed clerk webhook onUserCreateModel to match the real response

--- src/app/api/webhooks/onUserCreate/model.ts
+++ src/app/api/webhooks/onUserCreate/model.ts
@@ -1,38 +1,105 @@
-export interface OnUserCreateModel {
+export default interface OnUserCreateModel {
   data: Data
+  event_attributes: EventAttributes
   object: string
   type: string
 }
 
 interface Data {
-  data: Data2
-  delivered_by_clerk: boolean
-  from_phone_number: string
+  backup_code_enabled: boolean
+  banned: boolean
+  create_organization_enabled: boolean
+  created_at: number
+  delete_self_enabled: boolean
+  email_addresses: EmailAddress[]
+  external_accounts: ExternalAccount[]
+  external_id: unknown
+  first_name: string
+  has_image: boolean
   id: string
-  message: string
+  image_url: string
+  last_active_at: number
+  last_name: string
+  last_sign_in_at: unknown
+  locked: boolean
+  lockout_expires_in_seconds: unknown
+  mfa_disabled_at: unknown
+  mfa_enabled_at: unknown
   object: string
-  phone_number_id: string
-  slug: string
-  status: string
-  to_phone_number: string
-  user_id: string
-}
-
-interface Data2 {
-  app: App
-  otp_code: string
-  user: User
-}
-
-interface App {
-  domain_name: string
-  logo_image_url: string
-  logo_url: unknown
-  name: string
-  url: string
-}
-
-interface User {
+  passkeys: unknown[]
+  password_enabled: boolean
+  phone_numbers: unknown[]
+  primary_email_address_id: string
+  primary_phone_number_id: unknown
+  primary_web3_wallet_id: unknown
+  private_metadata: unknown
+  profile_image_url: string
   public_metadata: unknown
-  public_metadata_fallback: string
+  saml_accounts: unknown[]
+  totp_enabled: boolean
+  two_factor_enabled: boolean
+  unsafe_metadata: unknown
+  updated_at: number
+  username: string
+  verification_attempts_remaining: number
+  web3_wallets: unknown[]
+}
+
+interface EmailAddress {
+  created_at: number
+  email_address: string
+  id: string
+  linked_to: LinkedTo[]
+  object: string
+  reserved: boolean
+  updated_at: number
+  verification: Verification
+}
+
+export interface LinkedTo {
+  id: string
+  type: string
+}
+
+export interface Verification {
+  attempts: unknown
+  expire_at: unknown
+  status: string
+  strategy: string
+}
+
+export interface ExternalAccount {
+  approved_scopes: string
+  avatar_url: string
+  created_at: number
+  email_address: string
+  first_name: string
+  id: string
+  identification_id: string
+  image_url: string
+  label: unknown
+  last_name: string
+  object: string
+  provider: string
+  provider_user_id: string
+  public_metadata: unknown
+  updated_at: number
+  username: string
+  verification: Verification2
+}
+
+export interface Verification2 {
+  attempts: unknown
+  expire_at: number
+  status: string
+  strategy: string
+}
+
+export interface EventAttributes {
+  http_request: HttpRequest
+}
+
+export interface HttpRequest {
+  client_ip: string
+  user_agent: string
 }
--- src/app/api/webhooks/onUserCreate/route.ts
+++ src/app/api/webhooks/onUserCreate/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server'
 import { db } from '~/server/db'
-import { type OnUserCreateModel } from './model'
 import { users } from '~/server/db/schema'
 import { eq } from 'drizzle-orm'
+import type OnUserCreateModel from './model'
 
 async function handler(req: Request) {
   const json = await req.json() as OnUserCreateModel
-  const userId = json.data.user_id
+  const userId = json.data.id
 
   //! todo add user to the db
   const existringUser = await db.query.users.findFirst({ where: eq(users.id, userId) })
