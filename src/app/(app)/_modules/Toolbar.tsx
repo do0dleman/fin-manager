50bf2b4537aa16144d139d217566e290b7a4dbad
do0dleman <nikita.obrazcovs@gmail.com>
2024-08-09 13:57:37 +0300

    added links to the toolbar

--- src/app/(app)/_modules/Toolbar.tsx
+++ src/app/(app)/_modules/Toolbar.tsx
@@ -1,18 +1,48 @@
 "use client";
 
+import { House, LayoutDashboard, User } from "lucide-react";
+import Link from "next/link";
+import { Button } from "~/app/_components/ui/button";
 import { ResizablePanel } from "~/app/_components/ui/resizable";
-import { api } from "~/trpc/react";
 
 function Toolbar() {
-  const { data } = api.post.hello.useQuery({ text: "app" });
   return (
     <ResizablePanel
       defaultSize={10}
       minSize={5}
-      maxSize={20}
-      className="p-2 text-2xl"
+      maxSize={12}
+      className="p-2 pt-10 text-2xl"
     >
-      Toolbar: {data?.greeting ?? ""}
+      <Button
+        variant="ghost"
+        className="flex w-full justify-start gap-2 text-2xl"
+        asChild
+      >
+        <Link href="/app">
+          <LayoutDashboard className="" />
+          <span>Dashboard</span>
+        </Link>
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex w-full justify-start gap-2 text-2xl"
+        asChild
+      >
+        <Link href="/">
+          <House />
+          <span>Home</span>
+        </Link>
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex w-full justify-start gap-2 text-2xl"
+        asChild
+      >
+        <Link href="/account">
+          <User />
+          <span>Account</span>
+        </Link>
+      </Button>
     </ResizablePanel>
   );
 }
