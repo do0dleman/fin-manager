1056ca5b2362abbca2e75618ee5231cdbbda642b
do0dleman <nikita.obrazcovs@gmail.com>
2024-09-03 15:54:53 +0300

    added to app button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import { Button } from "./_components/ui/button";
+
 export default async function Home() {
-  return <main></main>;
+  return (
+    <main className="flex h-[100dvh] items-center justify-center">
+      <Button asChild>
+        <Link href="/app">To App</Link>
+      </Button>
+    </main>
+  );
 }
