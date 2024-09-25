b558e99f4356a2712ffdab44ad4be47b1fc071c8
do0dleman <nikita.obrazcovs@gmail.com>
2024-09-25 16:58:39 +0300

    hide to app button for users with inactive status

--- src/app/_components/AuthHeader.tsx
+++ src/app/_components/AuthHeader.tsx
@@ -4,16 +4,25 @@ import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "~/app/_components/ui/button";
 import AuthButton from "./AuthButton";
+import { api } from "~/trpc/react";
 
 function AuthHeader() {
   const { user, isSignedIn } = useUser();
+  const { data, isFetched } = api.users.getUserInfo.useQuery(
+    { user_id: user?.id ?? "" },
+    { enabled: user !== undefined },
+  );
 
   if (isSignedIn) {
     return (
       <div className="flex gap-4">
-        <Button asChild>
-          <Link href="/app">To App</Link>
-        </Button>
+        {data?.user.status !== "inactive" ? (
+          <Button asChild>
+            <Link href="/app">To App</Link>
+          </Button>
+        ) : (
+          <></>
+        )}
         <AuthButton style="avatar" />
       </div>
     );
