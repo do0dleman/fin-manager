f02a8ef28dcc2e8315af3d274f54e21d60cc1962
do0dleman <nikita.obrazcovs@gmail.com>
2024-10-01 09:08:02 +0300

    changed loader colors to colors from color system

--- src/app/(app)/_modules/Loader.tsx
+++ src/app/(app)/_modules/Loader.tsx
@@ -9,7 +9,7 @@ function Loader() {
   if (!isLoading) return;
 
   return (
-    <div className="fixed bottom-4 right-4 z-10 h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-gray-900" />
+    <div className="fixed bottom-4 right-4 z-10 h-12 w-12 animate-spin rounded-full border-4 border-t-primary" />
   );
 }
 
